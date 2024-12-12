21a37f487f5dcc5008529045b89a1fe6c9e0e3fa
Matthew Spriggs <matspriggs@gmail.com>
2024-12-12 15:36:43 +0000

    added s02 PSUB

--- metagenomics/scripts/s02_qc.sh
+++ metagenomics/scripts/s02_qc.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 # FastQC & MiltiQC
-# Alexey Larionov, 08Dec2024
+# Matthew Spriggs: 12Dec24
 # Requires FastQC and MultiQC in environment
+
+# PBS directives
+#---------------
+
+#PBS -N s02_qc
+#PBS -l nodes=1:ncpus=12
+#PBS -l walltime=00:30:00
+#PBS -q half_hour
+#PBS -m abe
+#PBS -M matthew.spriggs.452@cranfield.ac.uk
+
+#===============
+#PBS -j oe
+#PBS -v "CUDA_VISIBLE_DEVICES="
+#PBS -W sandbox=PRIVATE
+#PBS -k n
+ln -s $PWD $PBS_O_WORKDIR/$PBS_JOBID
+## Change to working directory
+cd $PBS_O_WORKDIR
+## Calculate number of CPUs and GPUs
+export cpus=`cat $PBS_NODEFILE | wc -l`
+## Load production modules
+module use /apps2/modules/all
+## =============
 
 # Stop at runtime errors
 set -e
+
+# Load required modules (this is an example, change it!)
+module load FastQC/0.11.9-Java-11
+module load MultiQC/1.12-foss-2021b
+fastqc --version
+multiqc --version
+
+# Base folder (this is an example, change it!)
+base_folder="/mnt/beegfs/home/s430452/metagenomics_assay/metagenomics/"
 
 # Start message
 echo "FastQC & MultiQC"
@@ -30,3 +63,8 @@ multiqc .
 # Completion message
 echo "Done"
 date
+
+## Tidy up the log directory
+## =========================
+rm $PBS_O_WORKDIR/$PBS_JOBID
+
