59e6a7bca7b537bf3f49c9af74284bf690fe42d2
Matthew Spriggs <matspriggs@gmail.com>
2024-12-13 10:25:18 +0000

    added s07 PBS

--- metagenomics/scripts/s07_q2_calculate_diversity_metrics.sh
+++ metagenomics/scripts/s07_q2_calculate_diversity_metrics.sh
@@ -1,3 +1,34 @@
+#!/bin/bash
+# Matthew Spriggs: 13Dec24
+
+# Crescent2 script
+# Note: this script should be run on a compute node
+# qsub script.sh
+
+# PBS directives
+#---------------
+
+#PBS -N s07_q2_calculate_diversity_metrics
+#PBS -l nodes=1:ncpus=12
+#PBS -l walltime=00:30:00
+#PBS -q half_hour
+#PBS -m abe
+#PBS -M matthew.spriggs.452@cranfield.ac.uk
+
+#===============
+#PBS -j oe
+#PBS -v "CUDA_VISIBLE_DEVICES="
+#PBS -W sandbox=PRIVATE
+#PBS -k n
+ln -s $PWD $PBS_O_WORKDIR/$PBS_JOBID
+## Change to working directory
+cd $PBS_O_WORKDIR
+## Calculate number of CPUs and GPUs
+export cpus=`cat $PBS_NODEFILE | wc -l`
+## Load production modules
+module use /apps2/modules/all
+## =============
+
 # QIIME2 - Calculate multiple diversity metrics
 # Alexey Larionov, 08Dec2024
 # Requires environment with QIIME2 
@@ -5,22 +36,34 @@
 # Stop at runtime errors
 set -e
 
+
+# Load required modules
+module load QIIME2/2022.8
+
+
 # Start message
 echo "QIIME2: Calculate multiple diversity metrics"
 date
 echo ""
 
 # Folders
-# base_folder="..."
+base_folder="/mnt/beegfs/home/s430452/metagenomics_assay/metagenomics"
 results_folder="${base_folder}/results"
 diversity_metrics_folder="${results_folder}/s07_diversity_metrics"
+
+# Remove results, if existed
+rm -fr "${diversity_metrics_folder}"
+
+# Make results folder
+mkdir -p "${diversity_metrics_folder}"
+
 
 # Calculate a whole bunch of diversity metrics 
 # Select the sampling-depth as the minimal count of non-chimeric reads (see output of step 4)
 qiime diversity core-metrics-phylogenetic \
 --i-table "${results_folder}/s04_table_dada2.qza" \
 --i-phylogeny "${results_folder}/s05_rooted_tree.qza" \
---p-sampling-depth ENTER_THE_DEPTH_HERE \
+--p-sampling-depth 14107 \
 --m-metadata-file "samples.txt" \
 --output-dir "${diversity_metrics_folder}"
 
@@ -65,3 +108,7 @@ qiime tools export \
 echo ""
 echo "Done"
 date
+
+## Tidy up the log directory
+## =========================
+rm $PBS_O_WORKDIR/$PBS_JOBID
