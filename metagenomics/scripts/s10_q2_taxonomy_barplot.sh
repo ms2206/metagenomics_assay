1d587ce0ee26e291aefa915daca2ab860f5051e5
Matthew Spriggs <matspriggs@gmail.com>
2024-12-13 11:25:54 +0000

    added s10 PBS

--- metagenomics/scripts/s10_q2_taxonomy_barplot.sh
+++ metagenomics/scripts/s10_q2_taxonomy_barplot.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 # QIIME2 - Taxonomy barplot
-# Alexey Larionov, 08Dec2024
+# Matthew Spriggs: 13Dec24
 # Requires environment with QIIME2 
 
 # Assumes that the resources folder contains the claccifier recommended by QIIME2 for 515F/806R 16S region. 
@@ -8,8 +9,37 @@
 # cd "${resources_folder}"
 # wget https://data.qiime2.org/2022.8/common/gg-13-8-99-515-806-nb-classifier.qza
 
+# PBS directives
+#---------------
+
+#PBS -N s10_q2_taxonomy_barplot
+#PBS -l nodes=1:ncpus=12
+#PBS -l walltime=00:30:00
+#PBS -q half_hour
+#PBS -m abe
+#PBS -M matthew.spriggs.452@cranfield.ac.uk
+
+#===============
+#PBS -j oe
+#PBS -v "CUDA_VISIBLE_DEVICES="
+#PBS -W sandbox=PRIVATE
+#PBS -k n
+ln -s $PWD $PBS_O_WORKDIR/$PBS_JOBID
+## Change to working directory
+cd $PBS_O_WORKDIR
+## Calculate number of CPUs and GPUs
+export cpus=`cat $PBS_NODEFILE | wc -l`
+## Load production modules
+module use /apps2/modules/all
+## =============
+
+
 # Stop at runtime errors
 set -e
+
+
+# Load required modules
+module load QIIME2/2022.8
 
 # Start message
 echo "QIIME2: Taxonomy barplot"
@@ -17,7 +47,7 @@ date
 echo ""
 
 # Folders
-# base_folder="..."
+base_folder="/mnt/beegfs/home/s430452/metagenomics_assay/metagenomics"
 results_folder="${base_folder}/results"
 resources_folder="${base_folder}/resources"
 
@@ -43,3 +73,8 @@ qiime taxa barplot \
 echo ""
 echo "Done"
 date
+
+## Tidy up the log directory
+## =========================
+rm $PBS_O_WORKDIR/$PBS_JOBID
+
